757c3f61a223e85d2476b5392721458e6eaa2631
finite-infinity <50870852+finite-infinity@users.noreply.github.com>
2021-07-23 10:22:24 +0800

    Update utils.py

--- generators/utils.py
+++ generators/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-
+#仿射变换
 def get_affine_transform(center,
                          scale,
                          output_size,
@@ -40,26 +40,36 @@ def get_affine_transform(center,
 
     return trans
 
+#画高斯半径
+#在设置GT box的heat map的时候，我们不能仅仅只在top-left/bottom-right的位置设置标签，因为除了GT框，绿色的框其实也能很好的包围目标。
+#所以如果在检测中得到想绿色的这样的框的话，我们也给它保留下来。
+#只要预测的corners在top-left/bottom-right点的某一个半径r内，并且其与GTbox的IOU大于一个阈值(一般设为0.7)，我们将将这些点的标签不直接置为0
+#那置为多少呢？可以通过一个温和的方式来慢慢过渡，所以采用二维的高斯核未尝不可。
 
+#定义辐射区域（即保留一定标签的区域）
+#椭圆
 def draw_gaussian(heatmap, center, radius_h, radius_w, k=1):
     diameter_h = 2 * radius_h + 1
-    diameter_w = 2 * radius_w + 1
-    gaussian = gaussian2D((diameter_h, diameter_w), sigma_w=diameter_w / 6, sigma_h=diameter_h / 6)
+    diameter_w = 2 * radius_w + 1  #辐射圆直径
+    gaussian = gaussian2D((diameter_h, diameter_w), sigma_w=diameter_w / 6, sigma_h=diameter_h / 6)   #定义高斯核卷积
 
-    x, y = int(center[0]), int(center[1])
+    x, y = int(center[0]), int(center[1])   #中心点坐标
 
     height, width = heatmap.shape[0:2]
+    
+    #上下左右距中心距离
+    left, right = min(x, radius_w), min(width - x, radius_w + 1)   #左端距x距离
+    top, bottom = min(y, radius_h), min(height - y, radius_h + 1)  #辐射框底端距y距离（不跑出图外）  顶端距y距离（不高于GT框）
 
-    left, right = min(x, radius_w), min(width - x, radius_w + 1)
-    top, bottom = min(y, radius_h), min(height - y, radius_h + 1)
-
-    masked_heatmap = heatmap[y - top:y + bottom, x - left:x + right]
-    masked_gaussian = gaussian[radius_h - top:radius_h + bottom, radius_w - left:radius_w + right]
+    masked_heatmap = heatmap[y - top:y + bottom, x - left:x + right]  #保留图的mask
+    ## 取[abs(radius_h - y):max(y, radius_h)+min(height - y, radius_h + 1), abs(radius_w - x):max(x, radius_w)+min(width - x, radius_w + 1)
+    masked_gaussian = gaussian[radius_h - top:radius_h + bottom, radius_w - left:radius_w + right] 
     if min(masked_gaussian.shape) > 0 and min(masked_heatmap.shape) > 0:  # TODO debug
         np.maximum(masked_heatmap, masked_gaussian * k, out=masked_heatmap)
     return heatmap
 
 
+#长宽半径相同的圆
 def draw_gaussian_2(heatmap, center, radius, k=1):
     diameter = 2 * radius + 1
     gaussian = gaussian2D_2((diameter, diameter), sigma=diameter / 6)
@@ -109,27 +119,30 @@ def gaussian_radius(det_size, min_overlap=0.7):
     return rh, rw
 
 
+ #三种情况的r（大、小、错位）
 def gaussian_radius_2(det_size, min_overlap=0.7):
     height, width = det_size
-
+    #错位 （b正负无所谓）
     a1 = 1
     b1 = (height + width)
     c1 = width * height * (1 - min_overlap) / (1 + min_overlap)
     sq1 = np.sqrt(b1 ** 2 - 4 * a1 * c1)
     r1 = (b1 + sq1) / 2
-
+    
+    #小
     a2 = 4
     b2 = 2 * (height + width)
     c2 = (1 - min_overlap) * width * height
     sq2 = np.sqrt(b2 ** 2 - 4 * a2 * c2)
     r2 = (b2 + sq2) / 2
-
+    
+    #大
     a3 = 4 * min_overlap
     b3 = -2 * min_overlap * (height + width)
     c3 = (min_overlap - 1) * width * height
     sq3 = np.sqrt(b3 ** 2 - 4 * a3 * c3)
     r3 = (b3 + sq3) / 2
-    return min(r1, r2, r3)
+    return min(r1, r2, r3)  #返回最小的辐射半径
 
 
 def get_3rd_point(a, b):
