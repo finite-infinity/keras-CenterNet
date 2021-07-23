56a6fcf2bbab8d0425393083cbe6d0a17525c520
finite-infinity <50870852+finite-infinity@users.noreply.github.com>
2021-07-23 16:18:15 +0800

    Update msra_resnet.py

--- models/msra_resnet.py
+++ models/msra_resnet.py
@@ -201,7 +201,9 @@ def centernet(num_classes, backbone='resnet50', input_size=512, max_objects=100,
 
     loss_ = Lambda(loss, name='centernet_loss')(
         [y1, y2, y3, hm_input, wh_input, reg_input, reg_mask_input, index_input]) #搭建Lambda层 计算loss ，继承tf.keras.layers.Layer并重写config更稳妥
-    #用model拟合loss？
+    
+    # 利用函数API，从Input开始，然后后续指定前向过程，最后根据输入和输出来建立模型
+    # 搭建训练用model
     model = Model(inputs=[image_input, hm_input, wh_input, reg_input, reg_mask_input, index_input], outputs=[loss_])
 
     # detections = decode(y1, y2, y3) hm wh reg
@@ -211,6 +213,6 @@ def centernet(num_classes, backbone='resnet50', input_size=512, max_objects=100,
                                          score_threshold=score_threshold,
                                          nms=nms,
                                          num_classes=num_classes))([y1, y2, y3])
-    prediction_model = Model(inputs=image_input, outputs=detections)
-    debug_model = Model(inputs=image_input, outputs=[y1, y2, y3])   #利用函数API，从Input开始，然后后续指定前向过程，最后根据输入和输出来建立模型
+    prediction_model = Model(inputs=image_input, outputs=detections)  # 测试（推断）用model
+    debug_model = Model(inputs=image_input, outputs=[y1, y2, y3])     # 用于生成测试数据的
     return model, prediction_model, debug_model
